b1f4e0be76b6347d962763e912d5d45bbea997df
muhammad-haris <harisqureshi415@gmail.com>
2024-09-28 22:26:49 -0700

    counter app using react

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const incrementCount=()=>{
+    setCount(count+1)
+ }
+ const decrementCount=()=>{
+  
+  count < 1 ? count == 0 : setCount(count-1)
+}
+  
+  const [count, setCount] = useState(0)
+   
+  return (
+    <>
+  <h1>COUNTER APP</h1>
+  <h3>{count}</h3>
+  <button onClick={incrementCount}>INCREMENT</button>  <button onClick={decrementCount}>DECREAMENT</button>
+    </>
+  )
+}
+
+export default App
